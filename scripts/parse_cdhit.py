f87b476f9577ef7597453105a64ee20fb88fac69
Taavi Päll <tapa741@gmail.com>
2018-11-12 19:27:18 +0200

    fix object name

--- scripts/parse_cdhit.py
+++ scripts/parse_cdhit.py
@@ -50,9 +50,9 @@ with open(snakemake.output[2], "w") as merged:
 joined = SeqIO.parse(gzip.open(snakemake.input[3], "rt"), "fastq")
 joined_ids = get_ids(joined)
 
-with open(snakemake.output[3]) as joined, open(snakemake.output[4]) as un:
+with open(snakemake.output[3]) as join, open(snakemake.output[4]) as un:
   for record in SeqIO.parse(snakemake.output[2], "fasta"):
         if record.id in joined_ids:
-            SeqIO.write(record, joined, 'fasta')
+            SeqIO.write(record, join, 'fasta')
         else:
             SeqIO.write(record, un, 'fasta')
