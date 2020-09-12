66cd18d83f8f6d103506e25798c76da89eb94024
Taavi Päll <tapa741@gmail.com>
2020-09-12 08:50:46 +0300

    prepend sample id to record id

--- scripts/fix_fasta.py
+++ scripts/fix_fasta.py
@@ -3,6 +3,7 @@ from Bio import SeqIO
 fixed_seqs = []
 for record in SeqIO.parse(snakemake.input[0], "fasta"):
     record.description = ""
+    record.id  = f"{snakemake.params[0]}_{record.id}"
     fixed_seqs.append(record)
 
 SeqIO.write(fixed_seqs, snakemake.output[0], "fasta")
